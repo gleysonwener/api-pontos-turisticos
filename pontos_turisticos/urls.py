736c9e205c19168ff5ef16a2e8fadebdaf970644
gleysonwener <gleysonwener3@gmail.com>
2022-08-06 10:26:30 -0300

    Primeiro commit api

---
+++ pontos_turisticos/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
+from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
+from avaliacoes.api.viewsets import AvaliacaoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'pontosturisticos', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracaoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
+router.register(r'avaliacoes', AvaliacaoViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
