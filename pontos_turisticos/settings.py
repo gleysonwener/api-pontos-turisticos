93e06ac14e88dad1ec31fb6bb7852390fe763b9f
gleysonwener <gleysonwener3@gmail.com>
2022-08-20 16:23:20 -0300

    deploy heroku

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 #DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = True
 
-ALLOWED_HOSTS = ['pontos-turisticos-gleyson.herokuapp.com/', 'localhost:8000']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
